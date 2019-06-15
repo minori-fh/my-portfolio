ebde140aa79aa9e6d1fbafe13d75a520e0f7f254
Minori Hashimoto <minori.fh@gmail.com>
2019-06-15 16:53:39 -0700

    add text onto cube 1 and 2 on mousehover

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -22,6 +22,35 @@ $(".cube").on("mouseover",function(){
         $("#cube-" + (parseFloat(cubeNumber)-4)).animate({width: "50px"}, 500)
 });
 
+//Animate text on cube-1 ABOUT ME
+$("#cube-1").on("mouseover", function(){
+
+    setTimeout(function(){
+        $("#cube-1").html("<a href='aboutMe.html'> ABOUT ME </a>")
+        $("#cube-1").css("justify-content", "center")
+    }, 500)
+
+});
+
+$("#cube-1").on("mouseleave", function(){
+    $("#cube-1").html("")
+});
+
+
+//Animate text on cube-1 WORK
+$("#cube-2").on("mouseover", function(){
+
+    setTimeout(function(){
+        $("#cube-2").html("<a href='work.html'> WORK </a>")
+        $("#cube-2").css("justify-content", "center")
+    }, 500)
+
+});
+
+$("#cube-2").on("mouseleave", function(){
+    $("#cube-2").html("")
+});
+
 
 
 }); //END: document ready function
