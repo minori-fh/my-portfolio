c242a93eae02d74409ff8c7467d9dbd059940730
Minori Hashimoto <minori.fh@gmail.com>
2019-06-15 20:21:47 -0700

    add event handlers on mouseover and mouseleave of project pictures

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 
 $("#hangman-links").hide()
+$("#expressflix-links").hide()
+$("#polytopia-links").hide()
+$("#sip-links").hide()
+$("#trivia-links").hide()
 
 
 //Variable declarations
@@ -84,6 +88,46 @@ $("#hangman-col").on("mouseover", function(){
 $("#hangman-col").on("mouseleave", function(){
     $("#hangman").attr("src","assets/images/hangman.png")
     $("#hangman-links").hide()
+})
+
+$("#expressflix-col").on("mouseover", function(){
+    $("#expressflix").attr("src","assets/images/blank.png")
+    $("#expressflix-links").show()
+})
+
+$("#expressflix-col").on("mouseleave", function(){
+    $("#expressflix").attr("src","assets/images/expressflix.png")
+    $("#expressflix-links").hide()
+})
+
+$("#polytopia-col").on("mouseover", function(){
+    $("#polytopia").attr("src","assets/images/blank.png")
+    $("#polytopia-links").show()
+})
+
+$("#polytopia-col").on("mouseleave", function(){
+    $("#polytopia").attr("src","assets/images/polytopia.png")
+    $("#polytopia-links").hide()
+})
+
+$("#sip-col").on("mouseover", function(){
+    $("#sip").attr("src","assets/images/blank.png")
+    $("#sip-links").show()
+})
+
+$("#sip-col").on("mouseleave", function(){
+    $("#sip").attr("src","assets/images/sip.png")
+    $("#sip-links").hide()
+})
+
+$("#trivia-col").on("mouseover", function(){
+    $("#trivia").attr("src","assets/images/blank.png")
+    $("#trivia-links").show()
+})
+
+$("#trivia-col").on("mouseleave", function(){
+    $("#trivia").attr("src","assets/images/trivia.png")
+    $("#trivia-links").hide()
 })
 
 }); //END: document ready function
