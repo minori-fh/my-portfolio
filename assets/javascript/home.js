deef296a62b104a89b10da89ab1ae5fd65c7969e
Minori Hashimoto <minori.fh@gmail.com>
2019-06-16 12:18:22 -0700

    add styling to cubes to allow animation to adjust to top and bottom

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -16,11 +16,15 @@ var cubeNumber = "";
 //Initial page line animation
 $(".cube").css("height","2px")
 $(".cube").css("width","1px")
+$(".cube").css("margin-top","150px")
 $(".cube").animate({width: "80px"}, 1000)
 
 
 setTimeout(function(){
     $("#myName-column").text("Hi, my name is Minori.")
+    $("#myName-column").css("margin-top", "150px")
+    $("#contact-row-index").css("margin-top", "50px")
+    $(".cube").css("margin-top","0px")
 }, 1000)
 
 setTimeout(function(){
@@ -28,10 +32,14 @@ setTimeout(function(){
 }, 3000)
 
 setTimeout(function(){
-    $("#cube-0").animate({height: "300px"}, 700)
-    $("#cube-1").animate({height: "300px"}, 850)
-    $("#cube-2").animate({height: "300px"}, 1000)
-    $("#cube-3").animate({height: "300px"}, 1150)
+    $("#cube-0").animate({height: "300px", top: -150, bottom: -200}, 600);
+    $("#cube-1").animate({height: "300px", top: -150, bottom: -200}, 800);
+    $("#cube-2").animate({height: "300px", top: -150, bottom: -200}, 1000);
+    $("#cube-3").animate({height: "300px", top: -150, bottom: -200}, 1200);
+    // $("#cube-0").animate({height: "300px"}, 700)
+    // $("#cube-1").animate({height: "300px"}, 850)
+    // $("#cube-2").animate({height: "300px"}, 1000)
+    // $("#cube-3").animate({height: "300px"}, 1150)
 }, 3000)
 
 //Animate cube width on mouseover
