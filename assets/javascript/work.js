0e5b393f7d025a81260149e585f8ebc08060c846
Minori Hashimoto <minori.fh@gmail.com>
2019-07-01 18:22:26 -0700

    create folder structure in images folder to organize images and gifs

--- assets/javascript/work.js
+++ assets/javascript/work.js
@@ -24,62 +24,62 @@ $(".tech-icon").on("mouseover", function(){
 
 //Event handler: mouseover on project images
 $("#hangman-col").on("mouseover", function(){
-    $("#hangman").attr("src","assets/images/blank.png")
+    $("#hangman").attr("src","assets/images/project-pictures/blank.png")
     $("#hangman-links").show()
 })
 
 $("#hangman-col").on("mouseleave", function(){
-    $("#hangman").attr("src","assets/images/hangman.png")
+    $("#hangman").attr("src","assets/images/project-pictures/hangman.png")
     $("#hangman-links").hide()
 })
 
 $("#expressflix-col").on("mouseover", function(){
-    $("#expressflix").attr("src","assets/images/blank.png")
+    $("#expressflix").attr("src","assets/images/project-pictures/blank.png")
     $("#expressflix-links").show()
 })
 
 $("#expressflix-col").on("mouseleave", function(){
-    $("#expressflix").attr("src","assets/images/expressflix.png")
+    $("#expressflix").attr("src","assets/images/project-pictures/expressflix.png")
     $("#expressflix-links").hide()
 })
 
 $("#polytopia-col").on("mouseover", function(){
-    $("#polytopia").attr("src","assets/images/blank.png")
+    $("#polytopia").attr("src","assets/images/project-pictures/blank.png")
     $("#polytopia-links").show()
 })
 
 $("#polytopia-col").on("mouseleave", function(){
-    $("#polytopia").attr("src","assets/images/polytopia.png")
+    $("#polytopia").attr("src","assets/images/project-pictures/polytopia.png")
     $("#polytopia-links").hide()
 })
 
 $("#sip-col").on("mouseover", function(){
-    $("#sip").attr("src","assets/images/blank.png")
+    $("#sip").attr("src","assets/images/project-pictures/blank.png")
     $("#sip-links").show()
 })
 
 $("#sip-col").on("mouseleave", function(){
-    $("#sip").attr("src","assets/images/sip.png")
+    $("#sip").attr("src","assets/images/project-pictures/sip.png")
     $("#sip-links").hide()
 })
 
 $("#trivia-col").on("mouseover", function(){
-    $("#trivia").attr("src","assets/images/blank.png")
+    $("#trivia").attr("src","assets/images/project-pictures/blank.png")
     $("#trivia-links").show()
 })
 
 $("#trivia-col").on("mouseleave", function(){
-    $("#trivia").attr("src","assets/images/trivia.png")
+    $("#trivia").attr("src","assets/images/project-pictures/trivia.png")
     $("#trivia-links").hide()
 })
 
 $("#liri-col").on("mouseover", function(){
-    $("#liri").attr("src","assets/images/blank.png")
+    $("#liri").attr("src","assets/images/project-pictures/blank.png")
     $("#liri-links").show()
 })
 
 $("#liri-col").on("mouseleave", function(){
-    $("#liri").attr("src","assets/images/liri.png")
+    $("#liri").attr("src","assets/images/project-pictures/liri.png")
     $("#liri-links").hide()
 })
 
